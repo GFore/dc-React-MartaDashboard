28e58d02b81c0be10f919d02d0ecf933a979d43a
Greg Foreman <gfore@mindspring.com>
2021-03-29 23:56:03 -0400

    destructure props

--- src/Dropdowns.js
+++ src/Dropdowns.js
@@ -1,32 +1,31 @@
 import React from 'react';
 
-const Dropdowns = props => {
-    // first value for refresh rate is 'STOPPED'
-    // for other dropdowns it is 'ALL'
-    const firstOption = (props.name === "Refresh Rate")
-        ? <option value="STOPPED">STOPPED</option>
-        : <option value="ALL">ALL</option>;
-        
-    // map remaining options to option elements
-    const optionList = props.opts.map((opt, index) => {
-        return <option key={index} value={opt} >{opt}</option>
-    });
-
+const Dropdowns = ({ handleChange, name, opts, selectedName }) => {
+  // first value for refresh rate is 'STOPPED'
+  // for other dropdowns it is 'ALL'
+  const firstOption = (name === "Refresh Rate")
+    ? <option value="STOPPED">STOPPED</option>
+    : <option value="ALL">ALL</option>;
     
-    return (  
-        <div>
-            <label>{props.name}:  
-                <select 
-                    name={props.name} 
-                    value={props.selectedName} 
-                    onChange={(event) => props.handleChange(event)}
-                >
-                    {firstOption}
-                    {optionList}
-                </select>
-            </label>
-        </div>
-    );
-  }
+  // map remaining options to option elements
+  const optionList = opts.map((opt, index) => {
+    return <option key={index} value={opt} >{opt}</option>
+  });
+  
+  return (  
+    <div>
+      <label>{name}:  
+        <select 
+          name={name} 
+          value={selectedName} 
+          onChange={event => handleChange(event)}
+        >
+          {firstOption}
+          {optionList}
+        </select>
+      </label>
+    </div>
+  );
+}
 
 export default Dropdowns;
